7a213ab7f40e2f92c6649c7c4c993d7cbb5779a8
sushmita-singh <sushmitas326@gmail.com>
2024-01-16 15:48:05 +0530

    Project 07 - Currency Converter Finished

--- 06CurrencyConverter/src/components/InputBox.jsx
+++ 06CurrencyConverter/src/components/InputBox.jsx
@@ -28,12 +28,10 @@ function InputBox({
                 onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
-            <div className=''>
-                <p>Currency Type</p>
+            <div className='w-1/2 flex flex-wrap justify-end text-right'>
+                <p className='text-black/40 mb-2 w-full'>Currency Type</p>
                 <select 
-                className=''
-                name="" 
-                id=""
+                className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
                 value={selectedCurrency}
                 onChange={(e) => {
                     onCurrencyChange && onCurrencyChange(e.target.value)
