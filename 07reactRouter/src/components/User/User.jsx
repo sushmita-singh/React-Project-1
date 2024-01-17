0d6ce3f7d616fe1b2b56b65d75de6b111aee28fd
sushmita-singh <sushmitas326@gmail.com>
2024-01-17 15:53:26 +0530

    Halfway through Project 07 - React Router App

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {useParams} from 'react-router-dom'
+
+export default function User() {
+    const {userid} = useParams()
+    return (
+        <div className="bg-orange-500 text-black text-3xl text-center py-5">USER {userid}</div>
+    )
+}
