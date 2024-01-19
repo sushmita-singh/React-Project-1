a15bb32170d13d725a9659663b80e3761c68945b
sushmita-singh <sushmitas326@gmail.com>
2024-01-19 15:40:20 +0530

    Project 07- React Router App finished and projext 08 - Mini Context started

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,7 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/Github/Github.jsx'
+import Github, {githubInfoLoader} from './components/Github/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,7 +19,7 @@ const router = createBrowserRouter(
       <Route path='user/' element={<User />}>
         <Route path=':userid' element={<User />} />
       </Route>
-      <Route path='github' element={<Github />} />
+      <Route loader={githubInfoLoader} path='github' element={<Github />} />
       <Route path='*' element={<div>Not Found</div>} />
     </Route>
   )
